a2d4b7a85bcd890174b5dfdecdc24abfe0edc1bd
oussamatou nawal <nawal.oussamatou@etu.univ-orleans.fr>
2016-03-16 19:21:07 +0100

    installation de FOSUserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new AD\SoireeBundle\ADSoireeBundle(),
             new AD\StageBundle\ADStageBundle(),
 			new AD\CoreBundle\ADCoreBundle(),
+			new FOS\UserBundle\FOSUserBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
