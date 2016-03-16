91b0acddb17c8c2f05e88d6c4664f577f14ed04a
Agboton Kilian <kilian.agboton@etu.univ-orleans.fr>
2016-03-17 00:17:53 +0100

    Correction dans le fichier config.yml de la configuration de FOSUserBundle

--- src/AD/InscriptionBundle/ADInscriptionBundle.php
+++ src/AD/InscriptionBundle/ADInscriptionBundle.php
@@ -4,6 +4,9 @@ namespace AD\InscriptionBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class ADInscriptionBundle extends Bundle
-{
+class ADInscriptionBundle extends Bundle{
+	
+	public function getParent(){
+        return 'FOSUserBundle';
+    }
 }
